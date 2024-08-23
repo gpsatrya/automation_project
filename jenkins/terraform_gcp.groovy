a9148cae8681f38e1f8fefbc1a23f70b0eac3ad2
Gde Panji Satrya <gpsatrya.713@gmail.com>
2024-08-24 00:36:17 +0700

    add var in command terraform

--- jenkins/terraform_gcp.groovy
+++ jenkins/terraform_gcp.groovy
@@ -25,13 +25,11 @@ pipeline {
             steps {
                 script {
                     dir('terraform') {
-                        sh 'printenv'
-                        sh 'echo $GOOGLE_APPLICATION_CREDENTIALS'
                         // Initialize Terraform
                         sh 'terraform init'
 
                         // Apply Terraform configuration to create VM
-                        sh 'terraform apply -auto-approve'
+                        sh 'terraform apply -var 'google_application_credentials=${GOOGLE_APPLICATION_CREDENTIALS}' -auto-approve'
                     }
                 }
             }
@@ -48,7 +46,7 @@ pipeline {
                         sh 'terraform init'
 
                         // Destroy Terraform-managed infrastructure
-                        sh 'terraform destroy -auto-approve'
+                        sh 'terraform destroy -var 'google_application_credentials=${GOOGLE_APPLICATION_CREDENTIALS}' -auto-approve'
                     }
                 }
             }
