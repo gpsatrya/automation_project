b2cb235a7fd7c2b5eda341b1f39adf7c6d136b8e
Gde Panji Satrya <gpsatrya.713@gmail.com>
2024-08-23 17:58:15 +0700

    add jenkins project

---
+++ jenkins/testing.groovy
@@ -0,0 +1,53 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Clone Repository') {
+            steps {
+                sh 'git config --global http.sslVerify false'
+                git branch:'main', credentialsId: 'credential-github', url: 'https://github.com/gpsatrya/automation_project.git'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                // Menjalankan perintah build (misalnya, compile kode)
+                sh 'echo "Building the project..."'
+                // Tambahkan perintah build lainnya di sini, misalnya: mvn clean install
+            }
+        }
+
+        stage('Test') {
+            steps {
+                // Menjalankan unit tests
+                sh 'echo "Running tests..."'
+                // Tambahkan perintah testing di sini, misalnya: mvn test
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                // Menjalankan perintah deploy
+                sh 'echo "Deploying the application..."'
+                // Tambahkan perintah deploy lainnya di sini
+                // Misalnya, deploy ke server:
+                // sh 'scp target/app.jar user@server:/path/to/deploy'
+            }
+        }
+    }
+
+    post {
+        always {
+            // Langkah-langkah yang selalu dilakukan, terlepas dari status pipeline
+            echo 'This will always run'
+        }
+        success {
+            // Langkah-langkah yang dilakukan jika pipeline sukses
+            echo 'Pipeline succeeded!'
+        }
+        failure {
+            // Langkah-langkah yang dilakukan jika pipeline gagal
+            echo 'Pipeline failed!'
+        }
+    }
+}
