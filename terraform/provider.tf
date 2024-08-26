745f9ffdd59e2104d847b1ae769ea89932c5ee19
Gde Panji Satrya <gpsatrya.713@gmail.com>
2024-08-26 17:38:26 +0700

    typo in provider terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,6 +12,6 @@ terraform {
 
 provider "google" {
   credentials = file(var.google_application_credentials)
-  project     = "var.id_project"
-  region      = "var.region"
+  project     = var.id_project
+  region      = var.region
 }
